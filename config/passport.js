4aae968db824cae977d1f28166993903c5a6289a
Khalid <timoncr9@gmail.com>
2018-12-31 15:03:38 +0200

    Saving google authenticated user to the database.

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+const User = mongoose.model('users');
+
 module.exports = function(passport) {
     passport.use(
         new GoogleStrategy({
@@ -10,8 +12,31 @@ module.exports = function(passport) {
             callbackURL: '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile);
+            // console.log(accessToken);
+            // console.log(profile);
+            const img = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+            const newUser = {
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                image: img
+            }
+
+            // Check for existing user
+            User.findOne({
+                googleID: profile.id
+            }).then(user => {
+                if (user) {
+                    // Returning found user, First parameter is null because its the error parameter which we don't happen to have here
+                    done(null, user);
+                } else {
+                    //Create user
+                    new User(newUser)
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
         })
     )
 }
