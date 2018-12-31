6333e42e1fa754961d5a752907074d37c2b0385a
Khalid <timoncr9@gmail.com>
2018-12-31 14:20:29 +0200

    Created the initial google oauth login setup.

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes loading
+const auth = require('./routes/auth');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('it works');
+});
+
+// Using routes
+app.use('/auth', auth);
+
+const port = 5000;
+
+app.listen(5000, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://khalid:123456k@ds143971.mlab.com:43971/jobzilla',
+    googleClientID: '733537140651-3fgfcqnijrcjvbuitrpp9pkdr55d3uoc.apps.googleusercontent.com',
+    googleClientSecret: 'Tec3HR731f33u68jP2J3Foed'
+}
