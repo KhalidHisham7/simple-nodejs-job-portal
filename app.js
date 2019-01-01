978887ac32ad70e106638c4edcc158c163a88da9
Khalid <timoncr9@gmail.com>
2019-01-01 14:22:10 +0200

    1- Installed body parser. 2- Handled form errorsat register. 3- Added a new partial.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -36,6 +37,12 @@ app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cookieParser());
 app.use(session({
--- routes/auth.js
+++ routes/auth.js
@@ -35,6 +35,30 @@ router.get('/register', ensureGuest, (req, res) => {
     res.render('auth/register', { layout: 'auth' });
 });
 
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if (req.body.password != req.body.password2) {
+        errors.push({ text: 'Passwords do not match' });
+    }
+    if (req.body.password.length < 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    }
+    if (errors.length > 0) {
+        res.render('auth/register', {
+            layout: 'auth',
+            errors: errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
 router.get('/logout', (req, res) => {
     req.logOut();
     res.redirect('/');
